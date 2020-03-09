9dd438f8e2f3ffb7b13b22cf1db06774c790500e
Samuel Flowers <475197@chrome.escg.ac.uk>
2020-03-09 19:49:24 +0000

    finished my tutorial

---
+++ Ibussiness Inc The Game/Assets/Scenes/Scripts/Mainmenuoptions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Mainmenuoptions : MonoBehaviour
+{
+    public static bool isloading = false;
+
+    public void Newgame()
+    {
+        SceneManager.LoadScene("Mainscene");
+    }
+
+    public void Loadgame()
+    {
+        isloading = true;
+        SceneManager.LoadScene("Mainscene");
+    }
+}
