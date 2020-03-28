ff1c01c33bc19a596c453fca1e46c4bf9d47fe8f
Samuel Flowers <475197@chrome.escg.ac.uk>
2020-03-28 13:07:15 +0000

    finish progress bar tutorial

---
+++ Ibussiness Inc The Game/Assets/Ibusiness The Game Assets/Scripts/Events/Progressbar.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Progressbar : MonoBehaviour
+{
+    public Slider positive;
+    private float targetprogress = 0f;
+
+    public float fillspeed = 0.5f;
+    private void awake()
+    {
+        positive = gameObject.GetComponent<Slider>();
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        incrementprogress(0.75f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (positive.value < targetprogress)
+            positive.value += fillspeed * Time.deltaTime;
+    }
+
+    public void incrementprogress(float newprogress)
+    {
+        targetprogress = positive.value + newprogress;
+    }
+}
